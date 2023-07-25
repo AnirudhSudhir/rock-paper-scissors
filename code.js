9ab119f86735b015253730189b491f17fd608d09
QuarterQuack <137381184+QuarterQuack@users.noreply.github.com>
2023-07-25 22:44:15 +0530

    Add playRound

--- code.js
+++ code.js
@@ -7,3 +7,27 @@ function getComputerChoice() {
     else
         return "Scissor";
 }
+
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.equalsIgnoreCase(computerSelection))
+        return "Draw!";
+    else if (playerSelection.equalsIgnoreCase("Rock")) {
+        if (computerSelection.equalsIgnoreCase("Paper"))
+            return "You Lose! Paper beats Rock";
+        else
+            return "You Win! Rock beats Scissor";
+    }
+    else if (playerSelection.equalsIgnoreCase("Paper")) {
+        if (computerSelection.equalsIgnoreCase("Scissor"))
+            return "You Lose! Scissor beats Paper";
+        else
+            return "You Win! Paper beats Rock";
+    }
+    else {
+        if (computerSelection.equalsIgnoreCase("Rock"))
+            return "You Lose! Rock beats Scissor";
+        else
+            return "You Win! Scissor beats Paper";
+    }
+}
